ff2ce0e917be5b288c3bde7f7e745555b2ba5439
itsalimanuel <itskhaloufali@gmail.com>
2023-01-31 23:11:41 +0400

    setup project

---
+++ src/layout/index.vue
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<template>
+    <div>
+        <header></header>
+        <routerView/>
+        <footer></footer>
+    </div>
+</template>
+
+<style lang="stylus">
+
+</style>
