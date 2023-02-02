ce9e218a520cd2e4fd224d3c7dc455f7d9cc79e8
itsalimanuel <itskhaloufali@gmail.com>
2023-02-02 21:32:45 +0400

    fix store path

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import pianaStore from './store'
+import pianaStore from './stores/main'
 
 import './assets/main.css'
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,8 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script>
+/** */
+</script>
+
+<template>
+    <div></div>
+</template>
