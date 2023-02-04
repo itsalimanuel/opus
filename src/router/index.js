fb5e0240c66572d58fb744a914f3b60b4592caae
itsalimanuel <itskhaloufali@gmail.com>
2023-02-04 13:34:48 +0400

    rm .git

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/login/index.vue'
 
 const router = createRouter({
